ed921f968f5d5801d2e60c10b20392f2d750e7a3
Brahim LOUKIL <brahim_loukil@carrefour.fr>
2020-02-04 23:23:47 +0100

    rabbit auto configuration

---
+++ src/main/java/com/carrefour/oms/OmsRabbitConfig.java
@@ -0,0 +1,8 @@
+package com.carrefour.oms;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OmsRabbitConfig {
+
+}
---
+++ src/test/java/com/carrefour/oms/OmsAmqpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carrefour.oms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmsAmqpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
